015fb2112559044a565e57159947fed3c03ce030
Amenallah Hsoumi <amenallah.hsoumi@gmail.com>
2018-09-26 12:12:17 +0100

    create post model

---
+++ models/post.model.js
@@ -0,0 +1,11 @@
+const { getDB } = require('../lib/db');
+
+const Post = getDB().define('post', {
+  title: Sequelize.STRING,
+  content: Sequelize.TEXT,
+  author: Sequelize.STRING
+});
+
+Post.sync();
+
+module.exports = Post;
